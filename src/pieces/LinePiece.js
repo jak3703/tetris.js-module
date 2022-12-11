9e004e3de17036cf17c59bc0001488ca075dc01f
Jacob Kobrak <jak3703@g.rit.edu>
2022-12-11 17:48:31 -0500

    implemented piece constructors

--- src/pieces/LinePiece.js
+++ src/pieces/LinePiece.js
@@ -1,9 +1,17 @@
+import Tetris from '../Tetris'
 import Piece from '../Piece'
 
 class LinePiece extends Piece {
 
 	constructor() {
-		
+		const width = Tetris.GRID_WIDTH
+		const centerTile = [Math.floor(width / 2), 3]
+		tLocs = []
+		tLocs.push(centerTile)
+		tLocs.push([centerTile[0], centerTile[1] - 1])
+		tLocs.push([centerTile[0], centerTile[1] - 2])
+		tLocs.push([centerTile[0], centerTile[1] - 3])
+		super(tLocs)
 	}
 
 	getSymbol() {
--- src/pieces/SPiece.js
+++ src/pieces/SPiece.js
@@ -1,9 +1,17 @@
+import Tetris from '../Tetris'
 import Piece from '../Piece'
 
 class SPiece extends Piece {
 
 	constructor() {
-		
+		const width = Tetris.GRID_WIDTH
+		const centerTile = [Math.floor(width / 2), 3]
+		tLocs = []
+		tLocs.push(centerTile)
+		tLocs.push([centerTile[0], centerTile[1] - 1])
+		tLocs.push([centerTile[0] - 1, centerTile[1]])
+		tLocs.push([centerTile[0] + 1, centerTile[1] - 1])
+		super(tLocs)
 	}
 
 	getSymbol() {
